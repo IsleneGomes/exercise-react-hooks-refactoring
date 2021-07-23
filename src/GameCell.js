c2b690022df8fea7f2dd1a396fe814213d4741af
IsleneGomes <islenegomes@gmail.com>
2021-07-23 20:32:34 -0300

    Exercise-three

--- src/GameCell.js
+++ src/GameCell.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './GameCell.css';
 import xImage from './x.png';
 import oImage from './o.svg';
 
-class GameCell extends React.Component {
-  render() {
-    const { content, onClick, id } = this.props;
-
+function GameCell({ content, onClick, id }) {
     if (content === 1) {
       return (
         <div
@@ -50,15 +46,5 @@ class GameCell extends React.Component {
       />
     );
   }
-}
 
-GameCell.propTypes = {
-  content: PropTypes.oneOf([0, 1, 2]),
-  onClick: PropTypes.func.isRequired,
-  id: PropTypes.number.isRequired,
-};
-
-GameCell.defaultProps = {
-  content: 0,
-};
 export default GameCell;
